bdc17541380222b0f5a6a35fb80ca94410feaae6
pedro-mjgt <pedro.mjgt@gmail.com>
2024-05-10 15:29:07 -0500

    commit inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: InicioComponent}
+];
