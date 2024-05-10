6a916eeda093defd4b579ddc86e9afefbb4ea245
pedro-mjgt <pedro.mjgt@gmail.com>
2024-05-10 17:59:14 -0500

    primer commit

---
+++ src/app/ejercicio1/ejercicio1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio1',
+  standalone: true,
+  imports: [],
+  templateUrl: './ejercicio1.component.html',
+  styleUrl: './ejercicio1.component.scss'
+})
+export class Ejercicio1Component {
+
+}
