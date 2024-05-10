3b532d6ca169597776e26ba95b243e0c951cfbba
pedro-mjgt <pedro.mjgt@gmail.com>
2024-05-10 18:28:21 -0500

    ejercicio 4 - 2do commit

--- src/app/ejercicio4/ejercicio4.component.ts
+++ src/app/ejercicio4/ejercicio4.component.ts
@@ -9,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class Ejercicio4Component {
 
+  numero = {
+    valor: 0,
+    factorial: 1,
+  }
+
+  mostrarResultado = false;
+
+  calcularFactorial() {
+    const numero = this.numero.valor;
+
+    if (numero === 0 || numero === 1) {
+      this.numero.factorial = 1;
+    } else {
+      this.numero.factorial = this.factorial(numero);
+    }
+
+    this.mostrarResultado = true;
+  }
+
+  factorial (n: number): number {
+    if (n === 0 || n === 1) {
+      return 1;
+    } else {
+      return n * this.factorial(n - 1);
+    }
+  }
+
 }
