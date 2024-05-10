f003a0f5480b349a7a9fde39d1985fb67e0dc575
pedro-mjgt <pedro.mjgt@gmail.com>
2024-05-10 18:23:05 -0500

    commit pull request

---
+++ src/app/ejercicio4/ejercicio4.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio4',
+  standalone: true,
+  imports: [],
+  templateUrl: './ejercicio4.component.html',
+  styleUrl: './ejercicio4.component.scss'
+})
+export class Ejercicio4Component {
+
+}
