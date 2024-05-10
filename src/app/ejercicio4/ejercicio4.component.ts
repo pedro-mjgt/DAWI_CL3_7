31043d3008856a087fef13c58bf46b74f8334942
pedro-mjgt <pedro.mjgt@gmail.com>
2024-05-10 18:31:11 -0500

    ejercicio 4 - 3er commit

--- src/app/ejercicio4/ejercicio4.component.ts
+++ src/app/ejercicio4/ejercicio4.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ejercicio4',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './ejercicio4.component.html',
   styleUrl: './ejercicio4.component.scss'
 })
