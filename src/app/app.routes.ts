c830dcd4d0588521f10db8f3a57a4663b58da682
joan <i202214610@cibertec.edu.pe>
2024-05-11 10:14:56 -0500

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { InicioComponent } from './inicio/inicio.component';
 import { Ejercicio1Component } from './ejercicio1/ejercicio1.component';
 import { Ejercicio4Component } from './ejercicio4/ejercicio4.component';
 import { Ejercicio3Component } from './ejercicio3/ejercicio3.component';
+import { Ejercicio2Component } from './ejercicio2/ejercicio2.component';
 
 export const routes: Routes = [
     {path: '', component: InicioComponent},
     {path: 'ejercicio1', component: Ejercicio1Component},
+    {path: 'ejercicio2', component: Ejercicio2Component},
     {path: 'ejercicio3', component: Ejercicio3Component},
     {path: 'ejercicio4', component: Ejercicio4Component}
 ];
