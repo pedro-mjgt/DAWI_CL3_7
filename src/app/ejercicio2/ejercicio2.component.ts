d2b2793f6ca088e323f6c2da5d1693da5eb84d7f
joan <i202214610@cibertec.edu.pe>
2024-05-11 10:19:51 -0500

    segundo commit

--- src/app/ejercicio2/ejercicio2.component.ts
+++ src/app/ejercicio2/ejercicio2.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ejercicio2',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './ejercicio2.component.html',
   styleUrl: './ejercicio2.component.scss'
 })
 export class Ejercicio2Component {
+  mes ={
+    numeroMes: 0,
+    nombreMes: ""
+  };
 
+  
+
+  obtenerNombreMes() {
+    const nombresMeses = [
+      'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+      'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+    ];
+
+    // Convertir numeroMes a Number
+    const numeroMesNumber = Number(this.mes.numeroMes);
+
+    if (numeroMesNumber >= 1 && numeroMesNumber <= 12) {
+      this.mes.nombreMes = nombresMeses[numeroMesNumber - 1];
+    } else {
+      this.mes.nombreMes = 'Número de mes inválido';
+    }
+  }
 }
